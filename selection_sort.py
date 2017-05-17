11ec83edbb59a0d9370b238cd28689dba7d76832
mateus <mateus.aluufc@gmail.com>
2017-05-17 20:01:45 -0300

    Add files via upload

--- selection_sort.py
+++ selection_sort.py
@@ -1,8 +1,10 @@
+# Troca valor A[x] por A[y]
 def swap(array, firstIndex,secondIndex ):
     aux = array[firstIndex]
     array[firstIndex] = array[secondIndex]
     array[secondIndex] = aux
 
+# Retorna o index do menor valor do array
 def indexOfMinimum(array, startIndex):
     minValue = array[startIndex]
     minIndex = startIndex
@@ -12,14 +14,15 @@ def indexOfMinimum(array, startIndex):
             minValue = array[i]
             minIndex = i
     return minIndex
-ab = [0,1,2,3,4,5,6]
+
+# Realiza a permutacao
 def selectioSort(array):
     for i in range(0,len(array)):
         minimo = indexOfMinimum(array,i)
+        # Troca o A[i] por A[minimo]
         swap(array,i,minimo)
-        swap(ab,i,minimo)
     print array
 
 array = [22, 11, 99, 88, 9, 7, 42];
+
 selectioSort(array)
-print ab
