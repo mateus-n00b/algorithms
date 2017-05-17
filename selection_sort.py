f48c725c02ee9663cc48dbc47d35d8179f558871
mateus <mateus.aluufc@gmail.com>
2017-05-17 19:58:54 -0300

    Add files via upload

--- selection_sort.py
+++ selection_sort.py
@@ -12,13 +12,14 @@ def indexOfMinimum(array, startIndex):
             minValue = array[i]
             minIndex = i
     return minIndex
-
+ab = [0,1,2,3,4,5,6]
 def selectioSort(array):
     for i in range(0,len(array)):
         minimo = indexOfMinimum(array,i)
         swap(array,i,minimo)
+        swap(ab,i,minimo)
     print array
 
 array = [22, 11, 99, 88, 9, 7, 42];
-
 selectioSort(array)
+print ab
