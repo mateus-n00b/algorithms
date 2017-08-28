a62ec3c0d73358d74d9c4f02b192d5113f994971
mateus <mateus.aluufc@gmail.com>
2017-08-28 13:33:12 -0300

    Update 16.1-4.py

--- greedy/16.1-4.py
+++ greedy/16.1-4.py
@@ -3,25 +3,25 @@ def pinta_vertice(G,v,colors):
     used = [-1 for i in xrange(len(colors))]
 
     for u in G[v]:
-        if colors[u] != -1:
-            used[colors[u]] = colors[u]
+        if colors[u] != -1: # check if color are in use 
+            used[colors[u]] = colors[u] 
 
     for i in xrange(len(colors)):
-        if used[i] == -1:
-            colors[v] = i
+        if used[i] == -1: # Looks for the first available color
+            colors[v] = i   # paint the vertice
             break
 
 def colorir_grafo(G):
     t = 0
-    colors = [-1 for i in xrange(len(G))]
+    colors = [-1 for i in xrange(len(G))] # Colors available
 
-    for i in xrange(len(G)):
+    for i in xrange(len(G)):    # Paint all vertices
         if colors[i] == -1:
             pinta_vertice(G,i,colors)
 
     used = [ 0 for i in xrange(len(colors))]
 
-    for i in xrange(len(used)):
+    for i in xrange(len(used)): # Return how many colors was used
         if used[colors[i]] == 0:
             used[colors[i]] = 1
             t+=used[colors[i]]
